adcb1d4778e96ab328c2d4cf611e5ec9e06b1509
한강희 <hanabcde2@gmail.com>
2024-02-05 20:17:19 +0900

    02-01~02-05

--- level1/모음집2.js
+++ level1/모음집2.js
@@ -14,3 +14,50 @@ function 최소직사각형(sizes) {
 // 최소직사각형([[60, 50], [30, 70], [60, 30], [80, 40]])//4000
 // 최소직사각형([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])	//120
 // 최소직사각형([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])//	133
+
+
+function 시저암호(s, n) {
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var answer = '';
+
+  for (var i = 0; i < s.length; i++) {
+    var text = s[i];
+    if (text == ' ') {
+      answer += ' ';
+      continue;
+    }
+    var textArr = upper.includes(text) ? upper : lower;
+    var index = textArr.indexOf(text) + n;
+    if (index >= textArr.length) index -= textArr.length;
+    answer += textArr[index];
+  }
+  return answer;
+}
+시저암호("AB", 1)//	"BC"
+시저암호("z", 1)	//"a"
+시저암호("a B z", 4)	//"e F d"
+
+const 가장가까운같은글자 = (s) => {
+  let stack = [];
+  let answer = [];
+  for (let i = 0; i < s.length; i++) {
+    answer.push(stack.includes(s[i]) ? stack.length - stack.lastIndexOf(s[i]) : -1);
+    stack.push(s[i]);
+  }
+  return answer;
+}
+
+// 가장가까운같은글자("banana") //	[-1, -1, -1, 2, 2, 2]
+// 가장가까운같은글자("foobar") //	[-1, -1, 1, -1, -1, -1]
+
+const 푸드파이트대회 = (food) => {
+  let answer = ''
+  for (let i = 1; i < food.length; i++) {
+    answer += (i + '').repeat(~~(food[i] / 2))
+  }
+  answer += '0' + [...answer].reverse().join('')
+  console.log(answer)
+}
+// 푸드파이트대회([1, 3, 4, 6]) // "1223330333221"
+// 푸드파이트대회([1, 7, 1, 2]) // "111303111"
--- 코테스터디/3주차.js
+++ 코테스터디/3주차.js
@@ -19,3 +19,35 @@ const merge = (intervals) => {
 
 // merge([[1, 4], [0, 2], [3, 5]]) // [1,5]
 
+function 입국심사(n, times) {
+  times.sort((a, b) => a - b);
+  let left = 1;
+  let right = n * times[times.length - 1];
+  let answer = right; // 최대값.
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    console.log('mid', mid)
+    let count = 0
+    times.forEach(value => {
+      count += Math.floor(mid / value); // 한 사람당 몇명 할 수 있는지
+      console.log('Math.floor(mid / value)', Math.floor(mid / value))
+      if (count >= n) {
+        answer = Math.min(mid, answer); // 최솟값
+        console.log('Math.min(mid, answer)', Math.min(mid, answer))
+        return;
+      };
+    });
+    console.log('count', count)
+    console.log('right', right)
+    console.log('left', left)
+    if (count >= n) {
+      right = mid - 1;
+    }
+    else {
+      left = mid + 1;
+    }
+  }
+  return answer;
+}
+입국심사(6, [7, 10]) // 28
+
